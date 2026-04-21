46c89c0822e408fabccb96bfdbb40c5c1df48b74
joshua Simfukwe <joshuasimfukwe101@gmail.com>
2026-04-21 20:08:18 +0200

    feat: add category model

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# Category
+class category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at =models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name    
+# Supplier
+# Product
+# ProductPrice
+# StockBatch
+# InventoryTransaction
