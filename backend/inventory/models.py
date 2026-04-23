bdc30acfd9f194864f874b50e4d10f5b44ba3b28
joshua Simfukwe <joshuasimfukwe101@gmail.com>
2026-04-23 03:56:00 +0200

    feat: add Supplier model

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -9,8 +9,22 @@ class category(models.Model):
     created_at =models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name    
+        return self.name
+
+   
 # Supplier
+class Supplier(models.Model):
+    name = models.CharField(max_length=150)
+    contact_person = models.CharField(max_length=150, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 # Product
 # ProductPrice
 # StockBatch
