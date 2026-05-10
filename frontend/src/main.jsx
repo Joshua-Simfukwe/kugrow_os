d7b25db87f6d1ac568b247f53f60aebbfa315cb7
joshua Simfukwe <joshuasimfukwe101@gmail.com>
2026-05-10 16:08:08 +0200

    feat: set up onboarding routing structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import POS from './pages/POS.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import AppRouter from "./routes/AppRouter";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <POS />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,3 +1,35 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import LoginPage from "../features/auth/pages/LoginPage";
+import SignupPage from "../features/auth/pages/SignupPage";
+import OrganizationSelectPage from "../features/auth/pages/OrganizationSelectPage";
+import CreateOrganizationPage from "../features/auth/pages/CreateOrganizationPage";
+
+import POS from "../pages/POS";
+
 export default function AppRouter() {
-  return null;
+  return (
+    <Routes>
+      {/* Authentication */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      {/* Organizations */}
+      <Route
+        path="/organizations"
+        element={<OrganizationSelectPage />}
+      />
+
+      <Route
+        path="/create-organization"
+        element={<CreateOrganizationPage />}
+      />
+
+      {/* POS */}
+      <Route path="/pos" element={<POS />} />
+
+      {/* Default Redirect */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
 }
